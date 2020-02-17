9cb69dcc2f26776f6338167bccc94e86946df84f
hekk-ryoga-yamauchi <ryoga.yamauchi@happyelements.co.jp>
2020-02-17 13:25:56 +0900

    フィールドの作成

---
+++ Assets/Scripts/GameInstaller.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using Zenject;
+
+public class GameInstaller : MonoInstaller
+{
+    public override void InstallBindings()
+    {
+        Container.Bind<string>().FromInstance("Hello World!");
+        
+        
+    }
+}
